adfb002ad1d50a4905a55c767cdfd0a289eb8a68
Ralf S. Engelschall <rse@engelschall.com>
2014-01-08 10:37:59 +0100

    provide initial website

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+
+/* global module: true */
+module.exports = function (grunt) {
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-bower-install-simple");
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        "bower-install-simple": {
+            options: {
+                color:       true,
+                production:  true,
+                directory:   "bower_components"
+            }
+        },
+        copy: {
+            "sanitize": {
+                src: [ "bower_components/sanitize.css/sanitize.css" ],
+                dest: "lib/sanitize/sanitize.css"
+            },
+            "jquery": {
+                src: [ "bower_components/jquery/jquery.js" ],
+                dest: "lib/jquery/jquery.js"
+            },
+            "typopro": {
+                files: [
+                    { expand: true, flatten: false, cwd: "bower_components/typopro/web",
+                      src: "TypoPRO-OpenSans/**", dest: "lib/typopro/" },
+                    { expand: true, flatten: false, cwd: "bower_components/typopro/web",
+                      src: "TypoPRO-DejaVu/**", dest: "lib/typopro/" },
+                    { expand: true, flatten: false, cwd: "bower_components/typopro/web",
+                      src: "TypoPRO-Journal/**", dest: "lib/typopro/" }
+                ]
+            }
+        },
+        clean: {
+            clean:     [ "lib" ],
+            distclean: [ "node_modules", "bower_components" ]
+        }
+    });
+
+    grunt.registerTask("default", [ "bower-install-simple", "copy" ]);
+};
+
---
+++ index.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function () {
+    $.get("https://raw.github.com/rse/html5-spa-essentials/master/html5-spa-essentials.txt", function (data) {
+        var list = $("<table></table>");
+
+        var lines = data.split(/\r?\n/);
+        var k = 0;
+        for (var i = 0; i < lines.length; i++) {
+            var m;
+            if (typeof lines[i+1] === "string" && lines[i+1].match(/^====/)) {
+                m = lines[i].split(/\s+/);
+                list.append(
+                    "<tr class=\"header\">\n" +
+                    "  <th class=\"area\">" + m[0].replace(/_/g, " ") + "</td>\n" +
+                    "  <th class=\"name\">" + m[1].replace(/_/g, " ") + "</td>\n" +
+                    "  <th class=\"id\">" + m[2].replace(/_/g, " ") + "</td>\n" +
+                    "  <th class=\"version\">" + m[3].replace(/_/g, " ") + "</td>\n" +
+                    "  <th class=\"date\">" + m[4].replace(/_/g, " ") + "</td>\n" +
+                    "  <th class=\"license\">" + m[5].replace(/_/, " ") + "</td>\n" +
+                    "</tr>\n"
+                );
+            }
+            else {
+                m = lines[i].match(/^(.+?):\s+(.+?)\s+(.+?)\s+(.+?)\s+\((.+?)\)\s+(.+?)\s+([^(]+)(?:\s+(\(\*\)))?$/);
+                if (m === null)
+                    continue;
+                var item = {
+                    area:    m[1],
+                    name:    m[2],
+                    bower:   m[3],
+                    version: m[4],
+                    date:    m[5],
+                    license: m[6],
+                    url:     m[7],
+                    rse:     (m[8] !== undefined)
+                };
+                var c = "row-" + k;
+                if (item.rse)
+                    c += " rse";
+                list.append(
+                    "<tr class=\"" + c + "\">\n" +
+                    "  <td class=\"area\">" + item.area + "</td>\n" +
+                    "  <td class=\"name\"><a href=\"" + item.url + "\">" + item.name + "</a></td>\n" +
+                    "  <td class=\"id\"><a href=\"http://sindresorhus.com/bower-components/#!/search/" + item.bower + "\">" + item.bower + "</a></td>\n" +
+                    "  <td class=\"version\">" + item.version + "</td>\n" +
+                    "  <td class=\"date\">" + item.date + "</td>\n" +
+                    "  <td class=\"license\">" + item.license + "</td>\n" +
+                    "</tr>\n"
+                );
+            }
+            k = (k + 1) % 2;
+        }
+        $(".list").append(list);
+    });
+});
+
